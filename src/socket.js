0f682791834ae4808675cd92ed56ecfa40c0e228
Tisha Khathurani <tishaak17@gmail.com>
2025-12-06 17:08:47 +0530

    feat(frontend): update socket client to use REACT_APP_API_URL for websocket connection

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 
-// Initialize socket connection once
-export const socket = io('http://localhost:4000', {
-  transports: ['websocket'], // Force websocket only
+const API_URL = process.env.REACT_APP_API_URL; 
+// Example: http://localhost:4000
+
+export const socket = io(API_URL, {
+  transports: ['websocket'], // Force WebSocket only
 });
